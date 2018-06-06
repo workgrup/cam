e4837475d4d3cd6ff2d1bad17660b263180825c2
Camilo <camilobar20@hotmail.com>
2018-06-05 19:20:20 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,40 @@
+     var app = {
+
+          initialize: function() {
+              this.bindEvents();
+          },
+         
+          bindEvents: function() {
+              var takePhoto = document.getElementById('takePhoto');
+              takePhoto.addEventListener('click', app.takePhoto, false);
+              var sendPhoto = document.getElementById('sendPhoto');
+              sendPhoto.addEventListener('click', app.sendPhoto, false);
+          },
+
+          sendPhoto: function() {
+              alert('Imagen enviada al servidor');
+          },
+
+          takePhoto: function(){
+              navigator.camera.getPicture(app.onPhotoDataSuccess, app.onFail, { quality: 20, 
+                  allowEdit: true, destinationType: navigator.camera.DestinationType.DATA_URL });
+          },
+
+          onPhotoDataSuccess: function(imageData) {
+         
+            var photo = document.getElementById('photo');
+
+            photo.style.display = 'block';
+
+            photo.src = "data:image/jpeg;base64," + imageData;
+
+            var sendPhoto = document.getElementById('sendPhoto');
+            sendPhoto.style.display = 'block';
+            
+          },
+
+          onFail: function(message) {
+            alert('Failed because: ' + message);
+          }
+
+      };
